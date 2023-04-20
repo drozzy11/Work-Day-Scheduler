cd28629265d83f4e6abba7501a4766e95024e048
drozzy11 <ruiz.diego071299@gmail.com>
2023-04-19 23:22:01 -0500

    Made edits to README and added potential functioin for color changes in color changes in java

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,23 +1,19 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+
+
+  // the code will not run until the browser has finished rendering all the elements
+  var settings = {};
+  dayjs.locale(settings);
+  // dayjs.locale will cause a wait until the dom is fully loaded 
+  $(function () {
+    // Get the current hour of the day using the dayjs library.
+    var currentTime = dayjs().format('H');
+  // The function below changes the color of each time block based on whether it's in the "past, present, or future" relative to the current hour.
+    function hourlyColor() {
+      $('.time-block').each(function() {
+        var blockHour = parseInt(this.id);
+        $(this).toggleClass('past', blockHour < currentTime);
+        $(this).toggleClass('present', blockHour === currentTime);
+        $(this).toggleClass('future', blockHour > currentTime);
+      });
+    }
+  
